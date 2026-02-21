cb536cfe989469b3c5b61d8c17f81accb843b312
Adones Balica <adonesbalica@gmail.com>
2026-02-20 21:21:33 -0300

    feat: implement admin course management pages and forms; update dependencies

---
+++ lib/zodSchemas.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+
+export const coursesStatus = ["Draft", "Published", "Archived"] as const;
+
+export const courseCategories = [
+  "Development",
+  "Business",
+  "Finance",
+  "It & Software",
+  "Office productivity",
+  "Personal Development",
+  "Design",
+  "Marketing",
+  "Health & Fitness",
+  "Music",
+  "Teaching & Academics",
+] as const;
+
+export const courseSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be at least 3 characters long" })
+    .max(100, { message: "Title must be at most 100 characters" }),
+  description: z
+    .string()
+    .min(3, { message: "Description must be at least 3 characters long" }),
+
+  fileKey: z.string().min(1, { message: "File is required" }),
+
+  price: z.coerce.number().min(1, { message: "Price must be at least 1" }),
+
+  duration: z.coerce
+    .number()
+    .min(1, { message: "Duration must be at least 1 hours" })
+    .max(500, { message: "Duration must be at most 500 hours" }),
+
+  level: z.enum(courseLevels, {
+    message: "Level is required",
+  }),
+  category: z.enum(courseCategories, { message: "Category is required" }),
+  smallDescription: z
+    .string()
+    .min(3, {
+      message:
+        "Small Description description must be at least 3 characters long",
+    })
+    .max(200, {
+      message: "Small Description description must be at most 200 characters",
+    }),
+
+  slug: z
+    .string()
+    .min(3, { message: "Slug must be at least 3 characters long" }),
+
+  status: z.enum(coursesStatus, {
+    message: "Status is required",
+  }),
+});
+
+export type CourseSchemaType = z.infer<typeof courseSchema>;
