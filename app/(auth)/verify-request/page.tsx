ae34b8fbd2bf473996403cbc99f28900e30890eb
Adones Balica <adonesbalica@gmail.com>
2026-03-19 08:29:14 -0300

    fix: build error by dynamic pages

--- app/(auth)/verify-request/page.tsx
+++ app/(auth)/verify-request/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Loader } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useTransition } from "react";
